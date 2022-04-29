bd09e42172c16d3713e848bb1c66ad72a585a77a
xhw <1259747963@qq.com>
2022-04-29 16:41:44 +0800

    MOD: update

--- main.js
+++ main.js
@@ -4,35 +4,38 @@ const {
     utils: { enqueueLinks },
 } = Apify;
 
-const newsTitleStorage = {
-
-};
+const newsStorage = {};
 
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
     await requestQueue.addRequest({ url: 'https://bbc.com/news' });
 
+    const listTitleWrapClassName = '.nw-c-top-stories--standard .nw-c-top-stories__secondary-item .gs-o-faux-block-link__overlay-link';
     const handlePageFunction = async ({ request, $ }) => {
-        // const title = $('title').text();
-        // console.log(`The title of "${request.url}" is: ${title}.`);
         if (!request.userData.detailPage) {
-            const newTitle = $('.nw-c-top-stories--standard .nw-c-top-stories__secondary-item .gs-o-faux-block-link__overlay-link h3').text();
-            console.log(newTitle);
+            $(listTitleWrapClassName).each(function () {
+                let linkUrl = $(this).attr('href');
+                linkUrl = linkUrl.indexOf('https') > -1 ? linkUrl : `https://www.bbc.com${linkUrl}`;
+                newsStorage[linkUrl] = { title: $(this).find('h3').text() };
+            });
             await enqueueLinks({
                 $,
                 requestQueue,
-                selector: '.nw-c-top-stories--standard .nw-c-top-stories__secondary-item .gs-o-faux-block-link__overlay-link',
+                selector: listTitleWrapClassName, // TODO 没起作用
                 baseUrl: request.loadedUrl,
                 transformRequestFunction: (req) => {
                     req.userData.detailPage = true;
                     return req;
                 },
             });
+        } else {
+            newsStorage[request.url].content = $('.ssrcss-pv1rh6-ArticleWrapper').text(); // TODO 仅有文字内容，需要细化文章内容
+            console.log(newsStorage);
         }
     };
 
     const crawler = new Apify.CheerioCrawler({
-        maxRequestsPerCrawl: 20,
+        maxRequestsPerCrawl: 10,
         requestQueue,
         handlePageFunction,
     });
